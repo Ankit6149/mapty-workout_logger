5572892f6a7d6f7a4721b1f0ccdf5403915ab68b
Ankit Bhardwaj <97785108+Ankit6149@users.noreply.github.com>
2025-02-08 00:37:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -111,9 +111,8 @@ class App {
 
     this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
-    L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors',
     }).addTo(this.#map);
 
     this.#map.on('click', this._showForm.bind(this));
